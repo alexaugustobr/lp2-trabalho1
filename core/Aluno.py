1c26e07003d29667c0708d2f79ee722011e2994d
Michaelads <maike_maicon09@hotmail.com>
2017-11-04 23:08:39 -0200

    Feito classe alunos

--- core/Aluno.py
+++ core/Aluno.py
@@ -1,30 +1,28 @@
 from Pessoa import Pessoa
 from Usuario import Usuario
+
 class Aluno(Pessoa,Usuario):
     sigla_curso = None
     matriculas = []
 
+    def disciplinas_aluno(self):
+        nome_disciplinas = []
+        for matricula in self.matriculas:
+            nome_disciplinas.append(matricula.disciplina.nome)
+        return nome_disciplinas
+
+    def matricular(self, matricula):
+        if type(matricula) is not Matricula:
+            return False
+        if matricula.disciplina.nome != None and matricula.disciplina.nome in self.disciplinas_aluno():
+            return False
+
+        self.matriculas.append (matricula)
+        return True
+
+    def confirmar_matricular(self):
+        pass
 
 #NAO MOVER ESSE IMPORT CIRCULAR PARA CIMA
 from Disciplina import Disciplina
 from Matricula import Matricula
-
-def disciplinas_aluno(self):
-    nome_disciplinas = []
-    for matricula in matriculas:
-        nome_disciplinas.append(matricula.disciplina.nome)
-    
-    return nome_disciplinas
-
-def matricular(self, Matricula):
-    if type(matricula) is not Matricula:
-        return False
-    self.matricula = matricula
-    return True
-
-    for 
-
-  
-
-def confirmar_matricular(self):
-
