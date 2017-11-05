70b94f01207bc6b6b425927a7556739e81846db8
alexcologia33 <alexcologia33@gmail.com>
2017-11-04 22:25:30 -0200

    classe professor

--- core/Professor.py
+++ core/Professor.py
@@ -6,3 +6,24 @@ class Professor(Pessoa,Usuario):
     apelido = None
     disciplinas = []
 
+    def adicionar_disicplina(self, disciplina):
+        if type(disciplina) is not Disciplina:
+            return False
+        self.disciplinas.append(disciplina)
+        return True
+
+    def remove_disciplina(self, disciplina):
+        if not disciplina in disciplinas:
+            return False
+        disciplinas.remove(disciplina)
+        return True
+    
+    def disciplinas_professor(self):
+        return self.disciplinas
+
+    def carga_horaria_total(self):
+        cht = 0
+        for disciplina in disciplinas:
+            if disciplina.carga_horaria is int:
+                cht += disciplina.carga_horaria
+        return cht
