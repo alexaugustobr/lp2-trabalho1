e51bbd54b9bce159fc515113dabfcda69ac860ec
Michaelads <maike_maicon09@hotmail.com>
2017-11-04 22:32:01 -0200

    teste-aluno

--- core/Aluno.py
+++ core/Aluno.py
@@ -8,3 +8,23 @@ class Aluno(Pessoa,Usuario):
 #NAO MOVER ESSE IMPORT CIRCULAR PARA CIMA
 from Disciplina import Disciplina
 from Matricula import Matricula
+
+def disciplinas_aluno(self):
+    nome_disciplinas = []
+    for matricula in matriculas:
+        nome_disciplinas.append(matricula.disciplina.nome)
+    
+    return nome_disciplinas
+
+def matricular(self, Matricula):
+    if type(matricula) is not Matricula:
+        return False
+    self.matricula = matricula
+    return True
+
+    for 
+
+  
+
+def confirmar_matricular(self):
+
